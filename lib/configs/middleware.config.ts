0c62c1c106be78b9634f4b7d7e587bfb0dedaba3
An <anacelol1234@gmail.com>
2024-08-31 16:05:59 +0700

    setup middleware

---
+++ lib/configs/middleware.config.ts
@@ -0,0 +1,6 @@
+export const middlewareRoutes = {
+    publicRoutes: ["/", "/sign-in", "/sign-up"],
+    apiRoutes: ["/auth/sign-in", "/auth/sign-up"],
+    apiAuthPrefix: "/api/auth",
+    DEFAULT_SIGNIN_REDIRECT: "/sign-in",
+};
