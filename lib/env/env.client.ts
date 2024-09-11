4c66bd1f74e4a36710fe09337dc74327eefea3b7
An <anacelol1234@gmail.com>
2024-09-11 13:55:27 +0700

    skipping validation when CI

--- lib/env/env.client.ts
+++ lib/env/env.client.ts
@@ -14,4 +14,6 @@ export const envClient = createEnv({
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
         NODE_ENV: process.env.NODE_ENV,
     },
+    skipValidation:
+        !!process.env.CI || process.env.npm_lifecycle_event === "lint",
 });
