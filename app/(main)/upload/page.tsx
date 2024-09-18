2c395093fefb894c6d2f6bafc1fe9688db86d5b0
An <anacelol1234@gmail.com>
2024-09-18 14:14:33 +0700

    migrate google service to external service

--- app/(main)/upload/page.tsx
+++ app/(main)/upload/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { authApi } from "@/lib/cores/auth/apis";
 import { UploadButton } from "@/lib/cores/upload-btn";
 
 export default function UploadPage() {
+    const { data, isPending, isError, error } = authApi.query.useGetUser();
+    if (isError) {
+        <p>{error.message}</p>;
+    }
+    console.log(data);
     return (
         <div className="flex flex-col space-y-4">
-            UploadPage
             <UploadButton />
         </div>
     );
