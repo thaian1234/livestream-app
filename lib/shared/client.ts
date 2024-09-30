e44d384361d9c00d1a579a304e2cdcf26413835b
An <anacelol1234@gmail.com>
2024-09-30 09:18:54 +0700

    fix middleware for public routes

--- lib/shared/client.ts
+++ lib/shared/client.ts
@@ -1,6 +1,4 @@
 import { hc } from "hono/client";
-import { init } from "next/dist/compiled/webpack/webpack";
-import { set } from "zod";
 
 import { envClient } from "@/lib/env/env.client";
 
@@ -10,10 +8,8 @@ export const client = hc<AppType>(envClient.NEXT_PUBLIC_APP_URL);
 
 export const clientCookie = (cookies: string) => {
     const client = hc<AppType>(envClient.NEXT_PUBLIC_APP_URL, {
-        fetch(input, requestInit, Env, executionCtx) {
-            const headers = new Headers(requestInit?.headers);
-            headers.set("Cookie", cookies);
-            return fetch(input, { ...requestInit, headers });
+        headers: {
+            Cookie: cookies,
         },
     });
     return client;
