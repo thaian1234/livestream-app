28416185f9fa053f8f7b8026dc2de25b443a19e0
An <anacelol1234@gmail.com>
2024-09-30 10:05:04 +0700

    useDebounceCallback instead of useDebounceValue

--- lib/features/search/components/search-bar.tsx
+++ lib/features/search/components/search-bar.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Search } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import { useDebounceValue } from "usehooks-ts";
+import React from "react";
+import { useDebounceCallback } from "usehooks-ts";
 
 import { Input } from "@/components/ui/input";
 
 export function SearchBar() {
-    const [debounced, setValue] = useDebounceValue("", 500, { maxWait: 4 });
-    useEffect(() => {
-        console.log(debounced);
-    }, [debounced]);
+    const debounced = useDebounceCallback(handleChange, 500);
+    // useEffect(() => {
+    //     console.log(debounced);
+    // }, [debounced]);
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        console.log(e.target.value);
+        console.log("Waiting for 500ms");
+    }
     return (
         <div className="relative">
             <Input
@@ -18,8 +22,8 @@ export function SearchBar() {
                 placeholder="Search"
                 className="rounded-full border-0 bg-search pr-10 text-white placeholder:text-white/70 focus-visible:ring-white"
                 customSize="sm"
-                defaultValue={""}
-                onChange={(event) => setValue(event.target.value)}
+                defaultValue=""
+                onChange={debounced}
             />
             <Search className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white" />
         </div>
