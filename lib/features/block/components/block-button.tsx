ff7233c5e1b16d4746b01302f51e425a7e64f9dc
An <anacelol1234@gmail.com>
2024-10-01 09:14:53 +0700

    refactor block button

--- lib/features/block/components/block-button.tsx
+++ lib/features/block/components/block-button.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { blockApi } from "../apis";
-import {
-    Ban,
-    CircleSlash2,
-    LockKeyhole,
-    LockKeyholeOpen,
-    LockOpen,
-} from "lucide-react";
+import { CircleSlash2, LockOpen } from "lucide-react";
 import { useState } from "react";
+
+import { cn } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 
@@ -31,21 +27,29 @@ export function BlockButton({ blockerId, blockedId }: BlockButtonProps) {
             variant="outline"
             onClick={handleClick}
             disabled={isPending}
-            className={`group relative flex h-10 w-12 items-center justify-center overflow-hidden transition-all duration-300 ease-in-out hover:bg-primary-foreground/90`}
+            className="group relative flex h-10 w-12 items-center justify-center overflow-hidden transition-all duration-300 ease-in-out hover:bg-primary-foreground/90"
         >
             <div
-                className={`absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out ${isBlocking ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"} `}
+                className={cn(
+                    "absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out",
+                    isBlocking
+                        ? "translate-y-0 opacity-100"
+                        : "translate-y-full opacity-0",
+                )}
             >
-                <LockOpen
-                    className={`h-5 w-5 text-primary transition-all duration-300 ease-in-out group-hover:scale-125 group-hover:fill-primary group-hover:text-primary`}
-                />
+                <LockOpen className="h-5 w-5 text-primary transition-all duration-300 ease-in-out group-hover:scale-125 group-hover:fill-primary group-hover:text-primary" />
             </div>
             <div
-                className={`absolute inset-0 flex items-center justify-center space-x-2 transition-all duration-300 ease-in-out ${isBlocking ? "-translate-y-full opacity-0" : "translate-y-0 opacity-100"} `}
+                className={
+                    (cn(
+                        "absolute inset-0 flex items-center justify-center space-x-2 transition-all duration-300 ease-in-out",
+                    ),
+                    isBlocking
+                        ? "-translate-y-full opacity-0"
+                        : "translate-y-0 opacity-100")
+                }
             >
-                <CircleSlash2
-                    className={`h-5 w-5 text-primary transition-all duration-300 ease-in-out group-hover:scale-125 group-hover:text-red-500`}
-                />
+                <CircleSlash2 className="h-5 w-5 text-primary transition-all duration-300 ease-in-out group-hover:scale-125 group-hover:text-red-500" />
             </div>
         </Button>
     );
