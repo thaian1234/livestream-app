697a83b799c370ce1f876658f40b22cfd86933c8
An <anacelol1234@gmail.com>
2024-10-01 10:40:34 +0700

    enhanced search api

--- lib/features/search/apis/index.ts
+++ lib/features/search/apis/index.ts
@@ -24,6 +24,9 @@ export const searchApi = {
                         filterBy,
                     },
                 },
+                {
+                    enabled: filterBy !== "",
+                },
             );
         },
     },
