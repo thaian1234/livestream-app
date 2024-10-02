c4a6a46adde6af8dee8ac82503f528d13642184d
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-10-02 18:15:20 +0700

    UI live

--- lib/components/sidebar/index.tsx
+++ lib/components/sidebar/index.tsx
@@ -30,47 +30,6 @@ export function Sidebar() {
     const recommends = data?.data.recommends;
 
     return (
-<<<<<<< HEAD
-        <div
-            className={cn(
-                "h-2/4 w-16 flex-shrink-0 overflow-x-hidden overflow-y-hidden rounded-br-3xl rounded-tr-3xl bg-gradient-to-t from-black-2 via-teal-3 to-teal-2 transition-all duration-300 ease-in-out",
-                isOpen && "h-full w-72",
-            )}
-        >
-            {isOpen ? (
-                <Card className="w-auto p-4 pr-0">
-                    <CardTitle className="mb-2 mr-4 flex flex-row items-center justify-between text-xl">
-                        Channels
-                        <TooltipModel content="Collapse" side="right">
-                            <button onClick={onExpand}>
-                                <Menu color="#ffffff" strokeWidth={2.25} />
-                            </button>
-                        </TooltipModel>
-                    </CardTitle>
-                    {following && (
-                        <ItemLayout title="Following Channel" link="/home">
-                            <Following followings={following} />
-                        </ItemLayout>
-                    )}
-                    {recommends && (
-                        <ItemLayout title="Recommended Channel" link="/">
-                            <Recommend recommends={recommends} />
-                        </ItemLayout>
-                    )}
-                </Card>
-            ) : (
-                <div className="flex flex-col items-center py-3">
-                    <TooltipModel content="Expand" side="right">
-                        <button onClick={onCollapse}>
-                            <Menu
-                                color="#ffffff"
-                                strokeWidth={2.25}
-                                className="my-3"
-                            />
-                        </button>
-                    </TooltipModel>
-                    <CollapseSidebar />
-=======
         <div>
             {!isHideSidebar && (
                 <div
@@ -117,7 +76,6 @@ export function Sidebar() {
                             <CollapseSidebar />
                         </div>
                     )}
->>>>>>> 3646984 (UI live)
                 </div>
             )}
         </div>
