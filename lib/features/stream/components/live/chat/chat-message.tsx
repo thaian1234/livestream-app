24d0458e987c7238b1baf6ea7c3028c25fa23035
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-10-02 18:16:50 +0700

    UI live

---
+++ lib/features/stream/components/live/chat/chat-message.tsx
@@ -0,0 +1,24 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface ChatMessageProps {
+    avatar: string;
+    userName: string;
+    message: string;
+    // Add more props if needed...
+}
+export function ChatMessage({ avatar, userName, message }: ChatMessageProps) {
+    return (
+        <div className="flex w-full items-start space-x-2">
+            <button>
+                <Avatar className="h-7 w-7">
+                    <AvatarImage src={avatar} alt={userName} />
+                    <AvatarFallback />
+                </Avatar>
+            </button>
+            <div>
+                <div className="text-sm text-teal-2">{userName}</div>
+                <div className="mr-2 text-sm">{message}</div>
+            </div>
+        </div>
+    );
+}
--- lib/stores/store-sidebar.ts
+++ lib/stores/store-sidebar.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
 
 interface SidebarStore {
-    isOpen: boolean;
-    onExpand: () => void;
-    onCollapse: () => void;
+    isOpenSidebar: boolean;
+    isHideSidebar: boolean;
+    onExpandSidebar: () => void;
+    onCollapseSidebar: () => void;
+    onHideSidebar: () => void;
 }
 
 export const useSidebar = create<SidebarStore>((set) => ({
-    isOpen: true,
-    onExpand: () => set(() => ({ isOpen: false })),
-    onCollapse() {
-        set(() => ({ isOpen: true }));
+    isOpenSidebar: false,
+    isHideSidebar: false,
+    onExpandSidebar: () => set(() => ({ isOpenSidebar: false })),
+    onCollapseSidebar: () => {
+        set(() => ({ isOpenSidebar: true }));
+    },
+    onHideSidebar: () => {
+        set((state) => ({ isHideSidebar: !state.isHideSidebar }));
     },
 }));
