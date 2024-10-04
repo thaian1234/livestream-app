e9b90f40b2fc72ddcf9c7ce1c95c16078fff9118
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-10-04 18:45:02 +0700

    add clear input

--- lib/features/search/components/search-bar.tsx
+++ lib/features/search/components/search-bar.tsx
@@ -1,40 +1,54 @@
 "use client";
 
 import { searchApi } from "../apis";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 import React, { useState } from "react";
 import { useDebounceCallback } from "usehooks-ts";
 
 import { Input } from "@/components/ui/input";
 
 export function SearchBar() {
-    const debounced = useDebounceCallback(handleChange, 500);
+    const debounced = useDebounceCallback(searchText, 500);
     const [search, setSearch] = useState("");
+    const [input, setInput] = useState("");
     const { data, isPending, error } = searchApi.query.useSearch(
         "1",
         "10",
         search,
     );
-    if (!isPending) console.log(data);
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const search = e.target.value;
-        console.log(e.target.value);
-        setSearch(search);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+        debounced(e.target.value);
+    };
 
-        console.log("Waiting for 500ms");
+    //search
+    function searchText(value: string) {
+        setSearch(value);
+        console.log("Waiting for 500ms  " + value);
     }
+
+    const deleteSearchInput = () => {
+        setInput("");
+    };
 
     return (
         <div className="relative">
+            <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white" />
             <Input
-                type="search"
                 placeholder="Search"
-                className="rounded-full border-0 bg-search pr-10 text-white placeholder:text-white/70 focus-visible:ring-white"
+                className="rounded-full border-0 bg-search px-10 text-white placeholder:text-white/70 focus-visible:ring-white"
                 customSize="sm"
-                defaultValue=""
-                onChange={debounced}
+                value={input}
+                onChange={handleChange}
             />
-            <Search className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white" />
+            {input !== "" && (
+                <button
+                    className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-white"
+                    onClick={deleteSearchInput}
+                >
+                    <X size={20} />
+                </button>
+            )}
         </div>
     );
 }
