add0eab1f4176164b8d3b53b59be692b86160d88
An <anacelol1234@gmail.com>
2024-10-05 16:30:52 +0700

    add dotenv

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,10 +1,14 @@
+import { config } from "dotenv";
 import { handle } from "hono/vercel";
 
 import app from "@/server/api";
+
+config({
+    path: ".env",
+});
 
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
-// export const OPTIONS = handle(app);
