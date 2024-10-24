eaccb2dec2b9570862b27cc830023fc4e5f27813
An <anacelol1234@gmail.com>
2024-10-24 16:55:57 +0700

    fix chat component

--- app/(dashboard)/dashboard/[username]/stream/page.tsx
+++ app/(dashboard)/dashboard/[username]/stream/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import { Chat } from "@/lib/features/stream/components/chat";
+import { LocalChat } from "@/lib/features/stream/components/chat/local-chat";
 import { LocalLiveInformation } from "@/lib/features/stream/components/local-livescreen/local-live-information";
 import { LocalLivestreamPlayer } from "@/lib/features/stream/components/local-livescreen/local-livestream-player";
 
 export default function StreamPage() {
     return (
-        <section className="grid grid-cols-12 gap-x-4 gap-y-2 px-5">
+        <section className="grid grid-cols-12 gap-4 px-5">
             <div className="col-span-9 row-span-5">
                 <LocalLivestreamPlayer />
             </div>
             <div className="col-span-3 col-start-10 row-span-5 row-start-1">
-                <Chat />
+                <LocalChat />
             </div>
-            <div className="col-span-9 col-start-1 row-span-2 row-start-9">
+            <div className="col-span-9 col-start-1 row-span-2">
                 <LocalLiveInformation />
             </div>
         </section>
