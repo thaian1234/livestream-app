b6161c73a96c5d688a9431bc42734483e490a69d
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-11-29 10:21:37 +0700

    fix open chat

--- components/livestream-player-state.tsx
+++ components/livestream-player-state.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { VideoIcon, VideoOffIcon } from "lucide-react";
+import { ArrowLeftToLine, VideoIcon, VideoOffIcon } from "lucide-react";
 
+import { useLiveInfor } from "@/lib/stores/store-live-infor";
 import { cn } from "@/lib/utils";
+
+import { TooltipModel } from "./tooltip-model";
 
 interface LiveStreamPlayerStateProps {
     stateMessage: string;
@@ -15,8 +18,20 @@ export function LiveStreamPlayerState({
     isLoading = false,
     isError = false,
 }: LiveStreamPlayerStateProps) {
+    const { isOpenChatComponent, onToggleChatComponent } = useLiveInfor();
     return (
-        <div className="flex h-full items-center justify-center rounded-xl bg-gray-900/60">
+        <div className="relative flex h-full items-center justify-center rounded-xl bg-gray-900/60">
+            <div className="absolute inset-x-0 left-0 right-0 top-0 h-14 w-full bg-gradient-to-b from-black-0/60 to-transparent" />
+            <div className="absolute inset-x-0 right-4 top-0 flex h-14 justify-end">
+                {!isOpenChatComponent && (
+                    <TooltipModel content="Expand" side="bottom">
+                        <button onClick={onToggleChatComponent}>
+                            <ArrowLeftToLine />
+                        </button>
+                    </TooltipModel>
+                )}
+            </div>
+
             <div className="flex flex-col items-center justify-center space-y-4 text-muted-foreground">
                 {isError && <VideoOffIcon className="size-16" />}
                 {!isError && (
