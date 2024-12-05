a8e9eeefe7a18f30d62a8bfeb1b7e40de1b43802
An <anacelol1234@gmail.com>
2024-12-05 15:13:38 +0700

    adding category filter

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -3,11 +3,14 @@
 import { Loader2 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useQueryState } from "nuqs";
+import { useState } from "react";
 
 import { UserPreview } from "@/lib/components/profile/search/user-preview";
+import { useCategoryTree } from "@/lib/features/category/hooks/use-category-tree";
 import { searchApi } from "@/lib/features/search/apis";
 import { LivesPreview } from "@/lib/features/search/components/live-preview";
 
+import { CategoryFilter } from "@/components/category-filter";
 import {
     Pagination,
     PaginationContent,
@@ -144,47 +147,55 @@ export default function SearchPage() {
     };
 
     return (
-        <Tabs defaultValue="All" className="w-full px-10">
-            <TabsList className="mb-8 grid w-[300px] grid-cols-3 space-x-4 bg-black-1">
-                {tabs.map((tabName, index) => (
-                    <TabsTrigger
-                        className="w-auto rounded-full bg-search text-white data-[state=active]:bg-teal-2"
-                        value={tabName}
-                        key={index}
-                    >
-                        {tabName}
-                    </TabsTrigger>
-                ))}
-            </TabsList>
-            <TabsContent value="All">
-                <div className="space-y-4">
-                    <p className="text-2xl">User</p>
-                    <div className="flex max-w-[700px] flex-col">
-                        <UserPreview users={data.data.data.users} limit={4} />
-                    </div>
+        <section className="relative">
+            <div className="absolute right-1/4 top-0 flex min-w-72 items-center justify-center rounded-lg border border-slate-500 p-4">
+                <CategoryFilter />
+            </div>
+            <Tabs defaultValue="All" className="w-full px-10">
+                <TabsList className="mb-8 grid w-[300px] grid-cols-3 space-x-4 bg-black-1">
+                    {tabs.map((tabName, index) => (
+                        <TabsTrigger
+                            className="w-auto rounded-full bg-search text-white data-[state=active]:bg-teal-2"
+                            value={tabName}
+                            key={index}
+                        >
+                            {tabName}
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+                <TabsContent value="All">
+                    <div className="space-y-4">
+                        <p className="text-2xl">User</p>
+                        <div className="flex max-w-[700px] flex-col">
+                            <UserPreview
+                                users={data.data.data.users}
+                                limit={4}
+                            />
+                        </div>
 
-                    <p className="text-2xl">Channel</p>
+                        <p className="text-2xl">Channel</p>
+                        <div className="flex flex-col space-y-4">
+                            <LivesPreview streams={streams} />
+                        </div>
+                    </div>
+                </TabsContent>
+
+                <TabsContent value="User">
                     <div className="flex flex-col space-y-4">
                         <LivesPreview streams={streams} />
                     </div>
-                </div>
-            </TabsContent>
-
-            <TabsContent value="User">
-                <div className="flex flex-col space-y-4">
-                    <LivesPreview streams={streams} />
-                </div>
-            </TabsContent>
-            <TabsContent value="Channel">
-                <div className="flex max-w-[700px] flex-col">
-                    <UserPreview users={data.data.data.users} />
-                </div>
-            </TabsContent>
-            <PaginationComponent
-                currentPage={data.data.pagination.currentPage}
-                totalPages={data.data.pagination.totalPages}
-                onPageChange={handleChangePage}
-            />
-        </Tabs>
+                </TabsContent>
+                <TabsContent value="Channel">
+                    <div className="flex max-w-[700px] flex-col">
+                        <UserPreview users={data.data.data.users} />
+                    </div>
+                </TabsContent>
+                <PaginationComponent
+                    currentPage={data.data.pagination.currentPage}
+                    totalPages={data.data.pagination.totalPages}
+                    onPageChange={handleChangePage}
+                />
+            </Tabs>
+        </section>
     );
 }
