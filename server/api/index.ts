4ee78d5ceaa0a6bb7bec0494e7f5fa5a11815a0d
An <anacelol1234@gmail.com>
2025-02-12 16:59:21 +0700

    fix db

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,12 +1,10 @@
 import { Hono } from "hono";
 
-import Database from "../db";
 import { App } from "./app/app";
 
-const db = new Database();
 const honoApp = new Hono();
 
-const app = new App(honoApp, db);
+const app = new App(honoApp);
 const routes = app.setupRoutes();
 
 type AppType = typeof routes;
