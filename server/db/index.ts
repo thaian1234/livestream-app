69b6577b5e65b3fedbd9dc102b67720ba72939a3
An <anacelol1234@gmail.com>
2025-02-17 09:41:53 +0700

    update dependencies

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,4 +1,5 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import { envServer } from "@/lib/env/env.server";
 
@@ -10,13 +11,23 @@ class Database {
     public db;
     public adapter: CustomAdapter;
     public constructor() {
+        const pool = new Pool({
+            connectionString: envServer.DB_URL,
+            max: 20, // Maximum number of clients in the pool
+            idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
+            connectionTimeoutMillis: 2000, // How long to wait for a connection
+            query_timeout: 10000, // Query timeout in milliseconds
+            statement_timeout: 10000, // Statement timeout in milliseconds
+            lock_timeout: 10000, // Lock timeout in milliseconds
+            ssl: process.env.NODE_ENV === "production" ? true : false,
+        });
         this.db = drizzle({
-            connection: envServer.DB_URL,
             schema: {
                 ...tableSchemas,
                 ...tableRelations,
             },
             casing: "snake_case",
+            client: pool,
         });
         this.adapter = new CustomAdapter(
             this.db,
