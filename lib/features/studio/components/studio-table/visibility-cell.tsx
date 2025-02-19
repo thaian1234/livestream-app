61b45ae52dbd92f9a4059f628ff071f17e12e26a
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2025-02-19 18:22:12 +0700

    icon for visibility

--- lib/features/studio/components/studio-table/visibility-cell.tsx
+++ lib/features/studio/components/studio-table/visibility-cell.tsx
@@ -1,4 +1,5 @@
 import { Row } from "@tanstack/react-table";
+import { Earth, Link2, LockKeyhole } from "lucide-react";
 
 import {
     Select,
@@ -14,13 +15,36 @@ export function VisibilityCell({ row }: { row: Row<IVideo> }) {
     //call api to change visibility value in get all Video api
     return (
         <Select defaultValue={row.original.visibility}>
-            <SelectTrigger className="w-[110px]">
-                <SelectValue>{row.original.visibility}</SelectValue>
+            <SelectTrigger className="w-[135px]">
+                <SelectValue>
+                    <span className="flex flex-row items-center gap-2">
+                        {row.original.visibility === "Private" ? (
+                            <LockKeyhole size={20} />
+                        ) : row.original.visibility === "Unlisted" ? (
+                            <Link2 size={20} />
+                        ) : (
+                            <Earth size={20} />
+                        )}
+                        {row.original.visibility}
+                    </span>
+                </SelectValue>
             </SelectTrigger>
             <SelectContent>
-                <SelectItem value="Private">Private</SelectItem>
-                <SelectItem value="Unlisted">Unlisted</SelectItem>
-                <SelectItem value="Public">Public</SelectItem>
+                <SelectItem value="Private">
+                    <span className="flex flex-row items-center gap-2">
+                        <LockKeyhole size={20} /> Private
+                    </span>
+                </SelectItem>
+                <SelectItem value="Unlisted">
+                    <span className="flex flex-row items-center gap-2">
+                        <Link2 size={20} /> Unlisted
+                    </span>
+                </SelectItem>
+                <SelectItem value="Public">
+                    <span className="flex flex-row items-center gap-2">
+                        <Earth size={20} /> Public
+                    </span>
+                </SelectItem>
             </SelectContent>
         </Select>
     );
